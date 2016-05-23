67aa0368c3db1911130866a2ade465d8ad45250e
James J Balamuta <james.balamuta@gmail.com>
2016-05-23 00:23:20 -0500

    Changes: - Changed deploy setup.
    Added: Preamble

--- _deploy.sh
+++ _deploy.sh
@@ -1,10 +1,19 @@
+# Set git config information
 git config user.name "James Balamuta"
 git config user.email "balamut2@illinois.edu"
 
 # Clone the gh-pages repository
 git clone -b gh-pages \
-  https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git 
-cp -r ./_book/* ./
+  https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git \
+  book-output
+
+# Change book output directory
+cd book-output
+
+# Copy output
+cp -r ../_book/* ./
+
+# Add all files to the repo
 git add *
 git commit -a -m "Updating book (${TRAVIS_BUILD_NUMBER})"
 git push origin gh-pages
