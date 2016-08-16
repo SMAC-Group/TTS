6eaa3f5f00ae49ed5ce6df59a1ce838df39d3147
Stephane <stephane@illinois.edu>
2016-08-16 16:13:04 -0500

    Improved overall introduction and added basic time series model content

--- code/chapter/01_introduction.R
+++ code/chapter/01_introduction.R
@@ -1,8 +1,69 @@
+## @knitr example_AR1
+# This code simulate a gaussian random walk process
+n = 100                               # process length
+phi = 0.5                             # phi parameter
+sigma2 = 1                            # innovation variance
+Xt = gen.gts(AR1(phi = phi, sigma2 = sigma2), n = n)
+plot(Xt)
+
+## @knitr RW2d
+
+RW2dimension = function(steps = 100){
+  # Initial matrix
+  step_direction = matrix(0,steps+1,2)
+  
+  # Start random walk
+  for (i in 2:steps+1){
+    # Draw a random number from U(0,1)
+    rn = runif(1)
+    
+    # Go right if rn \in [0,0.25)
+    if (rn < 0.25) {step_direction[i,1] = 1}
+      
+    # Go left if rn \in [0.25,0.5)
+    if (rn >= 0.25 && rn < 0.5) {step_direction[i,1] = -1}
+    
+    # Go forward if rn \in [0.5,0.75)
+    if (rn >= 0.5 && rn < 0.75) {step_direction[i,2] = 1}
+    
+    # Go backward if rn \in [0.75,1]
+    if (rn >= 0.75) {step_direction[i,2] = -1}
+  }
+  
+  # Cumulate steps
+  position = cbind(cumsum(step_direction[,1]),cumsum(step_direction[,2]))
+  
+  # Plot results
+  plot(NA, xlim = range(position[,1]), ylim = range(position[,2]),
+       xlab = "X-position", ylab = "Y-position",
+       main = paste("2D random walk with", steps, "steps"))
+  grid()
+  lines(position, type = "l")
+  points(c(0,position[steps,1]),c(0,position[steps,2]), 
+         pch = 16, cex= 3, col = c("red","blue"))
+  legend("topleft", c("Start point","End point"),
+  pch = 16, pt.cex = 3, col = c("red","blue"), bty = "n",
+  bg = "white", box.col = "white", cex = 1.2)
+}
+
+# Plot 2D random walk with 10^2 and 10^4 steps
+set.seed(2)
+par(mfrow = c(1,2))
+RW2dimension(steps = 10^2)
+RW2dimension(steps = 10^4) 
+
 ## @knitr example_WN
 # This code simulate a gaussian white noise process
 n = 100                               # process length
 sigma2 = 1                            # process variance
 Xt = gen.gts(WN(sigma2 = 1), n = n)
+plot(Xt)
+
+## @knitr example_RW
+# This code simulate a gaussian random walk process
+n = 100                               # process length
+sigma2 = 1                            # innovation variance
+Xt = gen.gts(RW(sigma2 = sigma2), n = n)
 plot(Xt)
 
 ## @knitr example_highfreq
