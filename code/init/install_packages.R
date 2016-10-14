8a79258bffe98c7cff39167676cd027d76d7c56d
James Joseph Balamuta <balamut2@illinois.edu>
2016-10-13 19:24:22 -0500

    Added exts dependency

--- code/init/install_packages.R
+++ code/init/install_packages.R
@@ -1,12 +1,18 @@
 # Packages used within the book
 inst_pkgs = load_pkgs =  c("devtools","reshape2","ggplot2","astsa","ggfortify",
-                           "zoo","gmwm","highfrequency","timeDate","gridExtra",
+                           "zoo","highfrequency","timeDate","gridExtra",
                            "robcor", "quantmod", "reshape2")
 inst_pkgs = inst_pkgs[!(inst_pkgs %in% installed.packages()[,"Package"])]
 if(length(inst_pkgs)) install.packages(inst_pkgs)
+
+# Install from github
+devtools::install_github("SMAC-Group/gmwm")
 
 # Install from github
 devtools::install_github("SMAC-Group/imudata")
 
 # Install from github
 devtools::install_github("SMAC-Group/smacdata")
+
+# Install from github
+devtools::install_github("SMAC-Group/exts")
